a53b37d9806dddbccb6c8189d31581c41d9c2aef
LingYuWeiGuang <LingYuWeiGuang@outlook.com>
2024-05-30 23:31:17 +0800

    Initial commit

---
+++ src/minecraft_src/net/minecraft/client/multiplayer/ServerData.java
@@ -0,0 +1,191 @@
+package net.minecraft.client.multiplayer;
+
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.IChatComponent;
+import viamcp.common.gui.ExtendedServerData;
+
+public class ServerData implements ExtendedServerData
+{
+    public String serverName;
+    public String serverIP;
+
+    /**
+     * the string indicating number of players on and capacity of the server that is shown on the server browser (i.e.
+     * "5/20" meaning 5 slots used out of 20 slots total)
+     */
+    public String populationInfo;
+
+    /**
+     * (better variable name would be 'hostname') server name as displayed in the server browser's second line (grey
+     * text)
+     */
+    public String serverMOTD;
+
+    /** last server ping that showed up in the server browser */
+    public long pingToServer;
+    public int version = 47;
+
+    /** Game version for this server. */
+    public String gameVersion = "1.8.9";
+    public boolean field_78841_f;
+    public String playerList;
+    private ServerData.ServerResourceMode resourceMode = ServerData.ServerResourceMode.PROMPT;
+    private String serverIcon;
+
+    /** True if the server is a LAN server */
+    private boolean lanServer;
+
+    // ViaForgeMCP
+    private ProtocolVersion viaMCPVersion;
+
+    public ServerData(String name, String ip, boolean isLan)
+    {
+        this.serverName = name;
+        this.serverIP = ip;
+        this.lanServer = isLan;
+    }
+
+    /**
+     * Returns an NBTTagCompound with the server's name, IP and maybe acceptTextures.
+     */
+    public NBTTagCompound getNBTCompound()
+    {
+        NBTTagCompound nbttagcompound = new NBTTagCompound();
+
+        // ViaForgeMCP
+        if (viaMCPVersion != null) {
+            nbttagcompound.setString("viaMCPVersion", viaMCPVersion.getName());
+        }
+
+        nbttagcompound.setString("name", this.serverName);
+        nbttagcompound.setString("ip", this.serverIP);
+
+        if (this.serverIcon != null)
+        {
+            nbttagcompound.setString("icon", this.serverIcon);
+        }
+
+        if (this.resourceMode == ServerData.ServerResourceMode.ENABLED)
+        {
+            nbttagcompound.setBoolean("acceptTextures", true);
+        }
+        else if (this.resourceMode == ServerData.ServerResourceMode.DISABLED)
+        {
+            nbttagcompound.setBoolean("acceptTextures", false);
+        }
+
+        return nbttagcompound;
+    }
+
+    public ServerData.ServerResourceMode getResourceMode()
+    {
+        return this.resourceMode;
+    }
+
+    public void setResourceMode(ServerData.ServerResourceMode mode)
+    {
+        this.resourceMode = mode;
+    }
+
+    /**
+     * Takes an NBTTagCompound with 'name' and 'ip' keys, returns a ServerData instance.
+     */
+    public static ServerData getServerDataFromNBTCompound(NBTTagCompound nbtCompound)
+    {
+        ServerData serverdata = new ServerData(nbtCompound.getString("name"), nbtCompound.getString("ip"), false);
+
+        if (nbtCompound.hasKey("icon", 8))
+        {
+            serverdata.setBase64EncodedIconData(nbtCompound.getString("icon"));
+        }
+
+        if (nbtCompound.hasKey("acceptTextures", 1))
+        {
+            if (nbtCompound.getBoolean("acceptTextures"))
+            {
+                serverdata.setResourceMode(ServerData.ServerResourceMode.ENABLED);
+            }
+            else
+            {
+                serverdata.setResourceMode(ServerData.ServerResourceMode.DISABLED);
+            }
+        }
+        else
+        {
+            serverdata.setResourceMode(ServerData.ServerResourceMode.PROMPT);
+        }
+
+        // ViaForgeMCP
+        if (nbtCompound.hasKey("viaMCPVersion")) {
+            serverdata.setVersion(ProtocolVersion.getClosest(nbtCompound.getString("viaMCPVersion")));
+        }
+
+        return serverdata;
+    }
+
+    /**
+     * Returns the base-64 encoded representation of the server's icon, or null if not available
+     */
+    public String getBase64EncodedIconData()
+    {
+        return this.serverIcon;
+    }
+
+    public void setBase64EncodedIconData(String icon)
+    {
+        this.serverIcon = icon;
+    }
+
+    /**
+     * Return true if the server is a LAN server
+     */
+    public boolean isOnLAN()
+    {
+        return this.lanServer;
+    }
+
+    public void copyFrom(ServerData serverDataIn)
+    {
+        // ViaForgeMCP
+        if (serverDataIn instanceof ExtendedServerData) {
+            viaMCPVersion = serverDataIn.getVersion();
+        }
+
+        this.serverIP = serverDataIn.serverIP;
+        this.serverName = serverDataIn.serverName;
+        this.setResourceMode(serverDataIn.getResourceMode());
+        this.serverIcon = serverDataIn.serverIcon;
+        this.lanServer = serverDataIn.lanServer;
+    }
+
+    @Override
+    public ProtocolVersion getVersion() {
+        return viaMCPVersion;
+    }
+
+    @Override
+    public void setVersion(ProtocolVersion version) {
+        viaMCPVersion = version;
+    }
+
+    public static enum ServerResourceMode
+    {
+        ENABLED("enabled"),
+        DISABLED("disabled"),
+        PROMPT("prompt");
+
+        private final IChatComponent motd;
+
+        private ServerResourceMode(String name)
+        {
+            this.motd = new ChatComponentTranslation("addServer.resourcePack." + name, new Object[0]);
+        }
+
+        public IChatComponent getMotd()
+        {
+            return this.motd;
+        }
+    }
+}
---
+++ src/viamcp_src/viamcp/ViaMCP.java
@@ -0,0 +1,62 @@
+/*
+ * This file is part of ViaForgeMCP - https://github.com/MolokyMC/ViaForgeMCP
+ * Copyright (C) 2021-2024 FlorianMichael/EnZaXD <florian.michael07@gmail.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package viamcp;
+
+import viamcp.common.platform.VFPlatform;
+import viamcp.provider.ViaMCPGameProfileFetcher;
+import net.minecraft.client.Minecraft;
+import net.minecraft.realms.RealmsSharedConstants;
+import net.minecraft.util.Session;
+import net.raphimc.vialegacy.protocols.release.protocol1_8to1_7_6_10.providers.GameProfileFetcher;
+
+import java.io.File;
+import java.util.function.Supplier;
+
+public class ViaMCP implements VFPlatform {
+    
+    public static final ViaMCP PLATFORM = new ViaMCP();
+
+    @Override
+    public int getGameVersion() {
+        return RealmsSharedConstants.NETWORK_PROTOCOL_VERSION;
+    }
+
+    @Override
+    public Supplier<Boolean> isSingleplayer() {
+        return () -> Minecraft.getMinecraft().isSingleplayer();
+    }
+
+    @Override
+    public File getLeadingDirectory() {
+        return Minecraft.getMinecraft().mcDataDir;
+    }
+
+    @Override
+    public void joinServer(String serverId) throws Throwable {
+        final Session session = Minecraft.getMinecraft().getSession();
+
+        Minecraft.getMinecraft().getSessionService().joinServer(session.getProfile(), session.getToken(), serverId);
+    }
+
+    @Override
+    public GameProfileFetcher getGameProfileFetcher() {
+        return new ViaMCPGameProfileFetcher();
+    }
+    
+}
